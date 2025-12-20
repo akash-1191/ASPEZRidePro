96febe6b25b7de609234bef823b7ddef2b00c6f7
akash-1191 <158453744+akash-1191@users.noreply.github.com>
2025-12-20 15:31:07 +0530

    donee

--- Program.cs
+++ Program.cs
@@ -174,6 +174,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EZRide API v1");
+});
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
